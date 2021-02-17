a5c047690fc58e69e81c6357e6c49e71ba01e443
Lysgren <oscar.lysgren@gmail.com>
2021-02-17 14:58:35 +0100

    Updated Index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as API from '@/api/mock.js'
 
 Vue.use(Vuex)
 
@@ -12,10 +13,37 @@ export default new Vuex.Store({
     // övrig kund info i localstorage inloggad user
   },
   getters: {
-    getMenu: state => state.menu
+    getMenu: (state) => state.menu,
   },
   mutations: {
+    setMenu: (state, menuAPI) => state.menu = menuAPI.menu,
+    setUser: (state, user) => {
+      state.userName = user.name
+      state.userEmail = user.email
+    }
   },
   actions: {
-  },
+    fetchPosts: async ({ commit }) => {
+      await API.fetchProducts().then(data => commit('setMenu', data))
+    }, // Up and running
+    registerUser: async ({ commit }, userData) => {
+      API.registerUser(userData.name, userData.email)
+      commit('setUser', userData)
+    },
+    login: ({ commit }, loginData) => { 
+      let login = API.login(loginData.name, loginData.email)
+
+      if (login == true) {
+        commit('setUser', loginData)
+      } else {
+        console.log('Failed to login')
+      }
+    },
+    makeOrder: async (commit, orderData) => {
+      API.makeOrder(orderData.id, orderData.items)
+    },
+    fetchHistory: async (commit, userId) => {
+      API.fetchOrderHistory(userId)
+    }
+  }
 })

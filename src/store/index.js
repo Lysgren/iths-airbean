b3fa3817b352cd5514eece754573f41300e6e442
Phnome <soold@hotmail.se>
2021-02-17 15:27:42 +0100

    cart Store

--- src/store/index.js
+++ src/store/index.js
@@ -9,18 +9,21 @@ export default new Vuex.Store({
     menu: [],
     userName: 'Anon',
     userEmail: null,
-    orderHistory: {}
+    orderHistory: {},
+    cart: [],
     // övrig kund info i localstorage inloggad user
   },
   getters: {
     getMenu: (state) => state.menu,
+    getCart: (state) => state.cart
   },
   mutations: {
     setMenu: (state, menuAPI) => state.menu = menuAPI.menu,
     setUser: (state, user) => {
       state.userName = user.name
       state.userEmail = user.email
-    }
+    },
+    addItem: (state, change) => state.cart.push(change)
   },
   actions: {
     fetchPosts: async ({ commit }) => {
@@ -39,6 +42,7 @@ export default new Vuex.Store({
         console.log('Failed to login')
       }
     },
+    addItem: (commit, item) => commit('addItem', item),
     makeOrder: async (commit, orderData) => {
       API.makeOrder(orderData.id, orderData.items)
     },

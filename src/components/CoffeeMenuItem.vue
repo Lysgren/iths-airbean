9739f12073c3001e59507089cc57a3716ef66211
Malin S <1malin.soderstrom@gmail.com>
2021-02-18 13:36:17 +0100

    add to cart func

--- src/components/CoffeeMenuItem.vue
+++ src/components/CoffeeMenuItem.vue
@@ -33,7 +33,7 @@ export default {
           } else{this.active = false}
       },
       addToCart(){
-          console.log(this.menuItem)
+          this.$store.commit("addItem",this.menuItem)
       }
   }
 }

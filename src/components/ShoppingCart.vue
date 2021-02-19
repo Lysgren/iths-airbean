db9434412550bbb018903ee25729eb9157247328
hanna <hanna.aspblad@iths.se>
2021-02-19 10:36:02 +0100

    la till summering av pris och att item bara läggs till order om den har amount större än 0

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -3,12 +3,14 @@
     <div class="shopping-cart">
       <h1>Din beställning</h1>
       <CartItem v-for="item in cartItems" :key="item.id" v-bind:item="item" />
-      <span>
-        <p>Totalt: {{ totalPrice }} kr</p>
+      
+      <div class="summary"> 
+        <span class="total"><p>Total</p><p>{{ totalPrice }} kr</p></span>
         <p>Inklusive moms + drönarleverans</p>
-      </span>
+        
+      </div>
 
-      <button @click="submitOrder">take my money</button>
+      <button @click="submitOrder">Take my money!</button>
     </div>
   </div>
 </template>
@@ -32,18 +34,25 @@ export default {
     totalPrice() {
       let prices = [];
       let total = 0;
-      this.$store.state.cart.forEach((coffee) => prices.push(coffee.price));
+      this.$store.state.cart.forEach((coffee) => prices.push(coffee.price * coffee.amount));
 
-     total = prices.reduce((newValue, current) => newValue + current)
-      return total
+      total = prices.reduce((newValue, current) => newValue + current);
+      return total;
     },
   },
 
   methods: {
+    // submitOrder() {
+    //   this.$store.mutations.makeOrder
+    // },
+    //flytta till mutations i store och commita via method ovan
     submitOrder() {
-      console.log("place order");
-      //this.$store.actions.makeOrder
+
+      let order = this.$store.state.cart.filter(item => item.amount >0)
+      //push order till databasen
+      console.log(order);
     },
+  
   },
 };
 </script>
@@ -53,38 +62,37 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  width: 200px;
+  width: 85%;
   height: fit-content;
   background-color: white;
-  border: 2px solid black;
+  border-radius: 4px;
   position: absolute;
   left: 0;
   right: 0;
   margin-left: auto;
   margin-right: auto;
-  padding: 8px;
+  margin-top: 16px;
+  padding: 16px;
+}
+button{
+  border-radius: 20px;
+  width: fit-content;
+  border: none;
+  background-color: #2F2926;
+  padding: 12px;
+  color: white;
+  align-self: center;
+
 }
 
 
-
-
-
-
-.shopping-cart {
+.total{
+  font-weight: bold;
+  font-size: 1.8rem;
   display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 200px;
-  height: 350px;
-  background-color: white;
-  border: 2px solid black;
-  position: absolute;
-  left: 0;
-  right: 0;
-  margin-left: auto;
-  margin-right: auto;
-  padding: 8px;
+  justify-content: space-between;
 }
+
 
 
 </style>
